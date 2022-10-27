33829efe502d8c8bbf73ef86765f1fee50d3150d
ChrisLee2C <54211930+ChrisLee2C@users.noreply.github.com>
2022-10-28 00:04:31 +0800

    Read Videos

--- read.py
+++ read.py
@@ -4,3 +4,15 @@ img = cv.imread('Photos/images.jfif')
 cv.imshow('Human', img)
 
 cv.waitKey(0)
+
+capture = cv.VideoCapture('Videos\Video Of Funny Cat.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
